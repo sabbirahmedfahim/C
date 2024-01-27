9ec597ef376c36ab1359dfa7eeb12c00d9275e82
Sabbir Ahmed Fahim <146383613+sabbirahmedfahim@users.noreply.github.com>
2024-01-28 00:24:34 +0600

    trying to compile codes for easy access

---
+++ Tutorial-CodeWithHarry/C/Tutorial15.c
@@ -0,0 +1,83 @@
+// ONE
+// i++ means i= i+1
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+    }
+
+
+// TWO
+// there is a silly difference between TWO and THREE. execute codes to understand fully
+/*
+#include <stdio.h>
+int main()
+{
+    int i=0, j=0;
+    for (; i < 5, j < 3; i++, j++)
+    {
+        printf("%d %d\n", i, j);
+    }
+
+    return 0;
+
+}
+*/
+
+// THREE
+/*
+#include <stdio.h>
+int main()
+{
+    int i=0, j=0;
+    for (; j < 3, i < 5; i++, j++)
+    {
+        printf("%d %d\n", i, j);
+        
+    }
+
+    return 0;
+
+}
+*/
+
+// FOUR
+/*
+#include <stdio.h>
+int main()
+{
+    int i, j=0;
+    for (i = 0; i < 5; i++, j++)
+    {
+        printf("%d %d\n", i, j);
+        
+    }
+
+    return 0;
+
+}
+*/
+
+// FIVE
+/*
+#include <stdio.h>
+int main()
+{
+    int i, j=0;
+    for (i = 0; i < 5;)
+    {
+        printf("%d %d\n", i, j);
+         i++, j++;
+    }
+
+    return 0;
+
+}
+*/
