e1cc7ce68f5b0b50ddc65aea51152e4a95900849
Sabbir Ahmed Fahim <146383613+sabbirahmedfahim@users.noreply.github.com>
2024-01-28 00:39:24 +0600

    Delete Tutorial-CodeWithHarry directory

--- Tutorial-CodeWithHarry/C/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-    {
-    printf("Enter the input character. q to quit
-    1. kms to miles\n
-    2. inches to foot\n
-    3. cms to inches\n
-    4. pound to kgs\n
-    5. inches to meters\n
-    ");
