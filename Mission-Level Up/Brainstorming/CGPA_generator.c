68b3d1af5f93dfdf45ae1ce9a44f92c357f656cf
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2024-02-10 00:11:58 +0600

    developed a cgpa calculator

--- Mission-Level Up/Brainstorming/CGPA_generator.c
+++ Mission-Level Up/Brainstorming/CGPA_generator.c
@@ -1,50 +1,5 @@
 //Build a program that wants your GPA & credit for indivisual courses & prints ultimate CGPA
 
-//way 1
-// #include <stdio.h>
-// int gpaAndCredit(int GPA, int credit) {
-//     return GPA*credit;
-//   }
-
-// int main()
-// {
-//     printf("%d", gpaAndCredit(3,4 ));
-//     printf("%d", gpaAndCredit(5,2));
-
-//    return 0;
-// }
-
-//way 2
-// #include <stdio.h>
-
-// int main()
-// {
-//     int total_sub;
-//     int total_credit;
-//     printf("Enter your CGPA &  credit(use space): ");
-//     scanf("%d %d", &total_sub, &total_credit);
-//    for (int i=0; i < total_sub; i++)
-//    {
-//     for (int j=0; j < total_credit; j++)
-//     {
-//         printf("%d %d", i*j);
-//     }
-    
-//    }
-   
-
-//    return 0;
-// }
-
-//way 3
-#include <stdio.h>
-
-int main()
-{
-   
-
-   return 0;
-}
 
 
 
